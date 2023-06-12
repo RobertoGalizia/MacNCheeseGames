59b65225fdaff7a5d85f4f9555134c40dce66f87
RobertoGalizia <72981575+RobertoGalizia@users.noreply.github.com>
2023-06-12 13:04:37 +0200

    First stage
    Added Player with movement and blast
    Created arena
    Missing only enemies

---
+++ MNCProgrammingTest/Assets/Scripts/PlayerBlast.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlast : MonoBehaviour
+{
+    [SerializeField] float rechargeTime = 1f;
+    [SerializeField] float blastRadius = 100f;
+    [SerializeField] float blastPower = 10f;
+
+    bool canBlast = true;
+    void Update()
+    {
+        if(canBlast)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                canBlast = false;
+                StartCoroutine(Recharge());
+                Blast();
+            }
+        }
+    }
+
+    IEnumerator Recharge()
+    {
+        yield return new WaitForSeconds(rechargeTime);
+        canBlast = true;
+    }
+    void Blast()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, blastRadius);
+        Vector2 thisPos = new Vector2(transform.position.x, transform.position.y);
+        foreach (Collider2D collider in colliders)
+        {
+            Rigidbody2D rb = collider.GetComponent<Rigidbody2D>();
+            if (rb == null) continue;
+            Vector2 otherPos = new Vector2(rb.gameObject.transform.position.x, rb.gameObject.transform.position.y);
+            Vector2 dir = (otherPos - thisPos).normalized;
+            rb.AddForce(dir * blastPower, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ MNCProgrammingTest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigidBody;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float rotationSpeed = 200f;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(transform.up * -vertical * movementSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(transform.forward, -horizontal * rotationSpeed * Time.deltaTime, Space.World);
+    }
+}
